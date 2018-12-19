2a0afdc8189204aeadfb8d938b8b05c70b87e303
Harrison <harrisonekpobimi@gmail.com>
2018-12-19 15:24:44 +0200

    add User model

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const server = restify.createServer();
 
 server.use(restify.plugins.bodyParser());
 server.listen(config.PORT, ()=> {
+  mongoose.set('useFindandModify', false);
   mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true });
 });
 
@@ -15,5 +16,6 @@ db.on('error', (err) => console.log(err));
 
 db.once('open', () => {
   require('./routes/customers')(server);
+  require('./routes/users')(server);
   console.log(`sever started on port ${config.PORT}`);
 });
